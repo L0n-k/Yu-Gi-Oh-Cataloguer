3ea5caa50e2ed24eddf54e0c51c80e740264dc85
Lonk <l0nk@protonmail.com>
2022-11-02 08:16:20 +0100

    javascript initials

---
+++ scripts/main.js
@@ -0,0 +1,26 @@
+let rowId = 0;
+let tablerow = document.createElement(div);
+tablerow.className="tablerow borderstyle";
+tablerow.id = rowId;
+let cardName = document.createElement(div);
+cardName.className = "tablecontent borderstyle name";
+let cardBaseType = document.createElement(div);
+cardBaseType.className = "tablecontent borderstyle cardtype";
+let cardAttribute = document.createElement(div);
+cardAttribute.className = "tablecontent borderstyle atribute";
+let cardLevel = document.createElement(div);
+cardLevel.className = "tablecontent borderstyle level";
+let cardScale = document.createElement(div);
+cardScale.className = "tablecontent borderstyle pendulum";
+let cardType = document.createElement(div);
+cardType.className = "tablecontent borderstyle type";
+let cardTuner = document.createElement(div);
+cardTuner.className = "tablecontent borderstyle tuner";
+let cardATK  = document.createElement(div);
+cardATK.className = "tablecontent borderstyle atk";
+let cardCount = document.createElement(div);
+cardCount.className = "tablecontent borderstyle count";
+let cardNotes = document.createElement(div);
+cardNotes.className = "tablecontent borderstyle notes";
+rowId++;
+
